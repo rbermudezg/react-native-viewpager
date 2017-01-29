c0588da3517d61bf76ccb9b19c5b31bad4467c7d
Rafael Bermúdez <rbermudezg@gmail.com>
2017-01-29 09:29:30 +0100

    Configure styles from parent view

--- ViewPager.js
+++ ViewPager.js
@@ -301,7 +301,7 @@ var ViewPager = React.createClass({
     });
 
     return (
-      <View style={{/*flex: 1*/}}
+      <View style={this.props.style}
         onLayout={(event) => {
             // console.log('ViewPager.onLayout()');
             var viewWidth = event.nativeEvent.layout.width;
